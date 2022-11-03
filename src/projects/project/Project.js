585fa3e1144ea63371a0c53cb35938737f4f3756
Diana Lipanova <altere2804@gmail.com>
2022-11-03 15:21:29 +0100

    The Title component added

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -4,13 +4,15 @@ import style from './Project.module.css';
 export const Project = (props) => {
     return (
         <div className={style.project}>
-            <div className={style.imgContainer}>
+            <div className={style.imgContainer} style={props.style}>
                 <a className={style.button} href="/#">Look at</a>
             </div>
-            <h3 className={style.title}>{props.title}</h3>
-            <span className={style.description}>
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle}>{props.title}</h3>
+                <span className={style.description}>
                 {props.description}
-            </span>
+                </span>
+            </div>
         </div>
     );
 }
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -2,12 +2,13 @@ import React from 'react';
 import style from './Skills.module.css';
 import styleContainer from '../../src/common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
+import {Title} from "../common/components/title/Title";
 
 export const Skills = () => {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-                <h2 className={style.title}>My Skills</h2>
+                <Title text={"My Skills"}/>
                 <div className={style.skills}>
                     <Skill title={"JS"} description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}/>
                     <Skill title={"CSS"} description={"It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}/>
