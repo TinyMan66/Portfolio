9935978e42f32743ac81ff38a8c928cd40c04261
Diana Lipanova <altere2804@gmail.com>
2022-11-03 23:01:29 +0100

    Styles to the Contacts component added

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import style from './Contacts.module.css';
+import style from './Contacts.module.scss';
 import styleContainer from '../../src/common/styles/Container.module.css'
+import {Title} from "../common/components/title/Title";
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <h2 className={style.title}>Contacts</h2>
+                <Title text={"Contacts"}/>
                 <form className={style.contactsForm}>
                     <input type="text"/>
                     <input type="text"/>
