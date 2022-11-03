15c698b5cafa70d6871de78ea351ee100108001b
Diana Lipanova <altere2804@gmail.com>
2022-11-03 15:23:49 +0100

    Styles and images added

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import style from './Projects.module.css';
 import styleContainer from '../../src/common/styles/Container.module.css'
-import {Project} from "./project/Project";
+import {Project} from './project/Project';
+import {Title} from '../common/components/title/Title';
+import todoImage from './../asserts/images/todolistFake.jpeg'
+import socialImage from './../asserts/images/socialFake.png'
 
 export const Projects = () => {
+    const social = {
+        backgroundImage: `url(${socialImage})`
+    };
+
+    const todoList = {
+        backgroundImage: `url(${todoImage})`
+    };
+
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <h2 className={style.title}>My Projects</h2>
+                <Title text={"My Projects"}/>
                 <div className={style.projects}>
-                    <Project title={"React"} description={"lorem jhdvjvhdvbdbvdv jhdghvhvdhvdjhv ddvd dhfdhbdvb"}/>
-                    <Project title={"CSS"} description={"lorem jhdvjvhdvbdbvdv jhdghvhvdhvdjhv"}/>
+                    <Project style={social} title={"Social Network"} description={"lorem jhdvjvhdvbdbvdv jhdghvhvdhvdjhv ddvd dhfdhbdvb"}/>
+                    <Project style={todoList} title={"Todo List"} description={"lorem jhdvjvhdvbdbvdv jhdghvhvdhvdjhv"}/>
                 </div>
             </div>
         </div>
