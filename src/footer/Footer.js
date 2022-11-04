4a1b43f5a0644a6cdb74c902c4804712d98d09a4
Diana Lipanova <altere2804@gmail.com>
2022-11-04 17:49:57 +0100

    Styles to the Footer component added

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import style from './Footer.module.css';
+import style from './Footer.module.scss';
 import styleContainer from '../../src/common/styles/Container.module.css'
+import {Title} from "../common/components/title/Title";
 
 export const Footer = () => {
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.title}>Diana Lipanova</h2>
+                {/*<h2 className={style.title}>Diana Lipanova</h2>*/}
+                <Title text={"Diana Lipanova"}/>
                 <div className={style.socialContainer}>
                     <div className={style.social}></div>
                     <div className={style.social}></div>
