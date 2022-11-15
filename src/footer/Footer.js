77c3e4cc9e4f6dc3cf006159bbe111e70a01243a
Diana Lipanova <altere2804@gmail.com>
2022-11-15 15:01:04 +0100

    Social media icons was added

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import style from './Footer.module.scss';
 import styleContainer from '../../src/common/styles/Container.module.css'
 import {Title} from "../common/components/title/Title";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faFacebook, faTelegram, faSquareWhatsapp, faWhatsapp, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
     return (
@@ -10,10 +12,11 @@ export const Footer = () => {
                 {/*<h2 className={style.title}>Diana Lipanova</h2>*/}
                 <Title text={"Diana Lipanova"}/>
                 <div className={style.socialContainer}>
-                    <div className={style.social}></div>
-                    <div className={style.social}></div>
-                    <div className={style.social}></div>
-                    <div className={style.social}></div>
+                    <div className={style.social}><FontAwesomeIcon icon={faLinkedin}/></div>
+                    <div className={style.social}><FontAwesomeIcon icon={faFacebook}/></div>
+                    <div className={style.social}><FontAwesomeIcon icon={faTelegram}/></div>
+                    <div className={style.social}><FontAwesomeIcon icon={faSquareWhatsapp}/></div>
+                    {/*<div className={style.social}><FontAwesomeIcon icon={faInstagram}/></div>*/}
                 </div>
                 <span>All rights reserved</span>
             </div>
