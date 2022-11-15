f588596511683c50a0069cc0f0ed1c8476ef3736
Diana Lipanova <altere2804@gmail.com>
2022-11-15 14:57:04 +0100

    The main photo was added

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import style from './Main.module.scss';
-import styleContainer from '../../src/common/styles/Container.module.css'
+import styleContainer from '../../src/common/styles/Container.module.css';
+import mainPhoto from './../asserts/images/mainPhoto.jpg';
 
 export const Main = () => {
+    const mPhoto = {
+        backgroundImage: `url(${mainPhoto})`
+    };
+
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={style.greeting}>
                     <span>Hi There </span>
-                    <h1>I am Diana Lipanova</h1>
+                    <h1>I'm Diana Lipanova</h1>
                     <p>A Frontend Developer</p>
                 </div>
-                <div className={style.photo}></div>
+                <div className={style.photo} style={mPhoto}></div>
             </div>
         </div>
     );
