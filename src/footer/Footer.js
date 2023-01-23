e4c02f4bc482759a84e5316e3550d32bd5aa0bc8
Diana Lipanova <altere2804@gmail.com>
2023-01-23 20:38:56 +0100

    feat: add social media links, change styles

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,8 +2,14 @@ import React from 'react';
 import style from './Footer.module.scss';
 import styleContainer from '../../src/common/styles/Container.module.css'
 import {Title} from "../common/components/title/Title";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faFacebook, faTelegram, faSquareWhatsapp, faWhatsapp, faInstagram} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+    faLinkedin,
+    faFacebook,
+    faTelegram,
+    faSquareWhatsapp,
+    faGithub
+} from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
     return (
@@ -11,13 +17,33 @@ export const Footer = () => {
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
                 <Title titleStyle={style.name} text={"Diana Lipanova"}/>
                 <div className={style.socialContainer}>
-                    <div className={style.social}><FontAwesomeIcon icon={faLinkedin}/></div>
-                    <div className={style.social}><FontAwesomeIcon icon={faFacebook}/></div>
-                    <div className={style.social}><FontAwesomeIcon icon={faTelegram}/></div>
-                    <div className={style.social}><FontAwesomeIcon icon={faSquareWhatsapp}/></div>
-                    {/*<div className={style.social}><FontAwesomeIcon icon={faInstagram}/></div>*/}
+                    <div className={style.socialIcon}>
+                        <a href="https://www.linkedin.com/in/diana-lipanova-b18395256/?locale=en_US">
+                            <FontAwesomeIcon icon={faLinkedin} className={style.icon}/>
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="https://www.facebook.com/profile.php?id=100013503166823">
+                            <FontAwesomeIcon icon={faFacebook} className={style.icon}/>
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="https://t.me/TinyMan6">
+                            <FontAwesomeIcon icon={faTelegram} className={style.icon}/>
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            <FontAwesomeIcon icon={faSquareWhatsapp} className={style.icon}/>
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="https://github.com/TinyMan66">
+                            <FontAwesomeIcon icon={faGithub} className={style.icon}/>
+                        </a>
+                    </div>
                 </div>
-                <span>All rights reserved</span>
+                <span className={style.copyright}> 2022 All rights reserved</span>
             </div>
         </div>
     );
