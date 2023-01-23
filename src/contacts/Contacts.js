93a86e1b1e15fd813a8865b58eef9fd56095f317
Diana Lipanova <altere2804@gmail.com>
2023-01-23 20:35:58 +0100

    refactor: add styles

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import style from './Contacts.module.scss';
-import styleContainer from '../../src/common/styles/Container.module.css'
 import {Title} from "../common/components/title/Title";
-import {Button} from "../common/components/button/Button";
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
-            <div className={`${styleContainer.container} ${style.contactsContainer}`}>
+            <div className={style.contactsContainer}>
                 <Title text={"Contacts"}/>
-                <form className={style.contactsForm}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea></textarea>
+                <form className={style.form}>
+                    <input type="text" className={style.formArea} placeholder="Name"/>
+                    <input type="text" className={style.formArea} placeholder="e-mail"/>
+                    <textarea className={style.messageArea} placeholder="Message"></textarea>
+                    <button type="submit">send message</button>
                 </form>
-                <Button path={"/#"} buttonName={"Send"}/>
             </div>
         </div>
     );
