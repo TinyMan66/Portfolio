2f01346c44c78b6cf595a873eda8bfb4a031d67f
Diana Lipanova <altere2804@gmail.com>
2023-01-23 11:20:24 +0100

    feat: add particles

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../../src/common/styles/Container.module.css';
-import mainPhoto from './../asserts/images/mainPhoto.jpg';
+import mainPhoto from '../assets/images/mainPhoto.jpg';
+import Particles from 'react-tsparticles';
+
+const particlesOpt = {
+    "particles": {
+        "number": {
+            "value": 150,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+    },
+};
 
 export const Main = () => {
     const mPhoto = {
@@ -10,6 +23,7 @@ export const Main = () => {
 
     return (
         <div className={style.mainBlock}>
+            <Particles className={style.particles} params={particlesOpt}/>
             <div className={styleContainer.container}>
                 <div className={style.greeting}>
                     <span>Hi There </span>
