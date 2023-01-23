a8590c75796ec3ee0019887f8ba4b3954474a58f
Diana Lipanova <altere2804@gmail.com>
2023-01-23 20:38:16 +0100

    refactor: add styles

--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -5,7 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const Skill = (props) => {
     return (
         <div className={styles.skill}>
-            <div className={styles.icon} style={props.style}> <FontAwesomeIcon icon={props.icon}/> </div>
+            <div className={styles.iconContainer} style={props.style}>
+                <FontAwesomeIcon icon={props.icon} className={styles.icon}/>
+            </div>
             <h3>{props.title}</h3>
             <span className={styles.description}>
                 {props.description}
