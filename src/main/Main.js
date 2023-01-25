10e19c6a70521c65c8bf614064be912b0421d03e
Diana Lipanova <altere2804@gmail.com>
2023-01-25 21:16:47 +0100

    feat: add particles

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,31 +1,54 @@
 import React from 'react';
 import style from './Main.module.scss';
-import Particles from 'react-tsparticles';
+import Particles from 'react-particles'
+import {useCallback} from "react";
+import {loadFull} from "tsparticles";
 
 const particlesOpt = {
     particles: {
         number: {
-            value: 150,
+            value: 80,
             density: {
                 enable: true,
                 value_area: 800
             }
         },
-    },
+        move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+    }
 };
 
 export const Main = () => {
+    const particlesInit = useCallback(async engine => {
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
 
     return (
         <div className={style.mainBlock}>
-            <Particles className={style.particles} params={particlesOpt}/>
+            <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded} className={style.particles} options={particlesOpt}/>
             <div className={style.container}>
                 <div className={style.greeting}>
                     <p>Hi There!</p>
                     <h1>
-                        I'm <span>Diana <span>Lipanova</span></span>
+                        I'm <span>Diana Lipanova</span>
                     </h1>
-                    <p>A Frontend Developer</p>
+                    <p>A Front-end Developer</p>
                 </div>
                 <div className={style.photo}>
                     <div className={style.image}></div>
