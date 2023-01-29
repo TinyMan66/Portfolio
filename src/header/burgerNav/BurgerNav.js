9c1aac6630614055c1795f731206005390a805aa
Diana Lipanova <altere2804@gmail.com>
2023-01-29 19:52:05 +0100

    feat: add burgerNav to header, and styles

---
+++ src/header/burgerNav/BurgerNav.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import style from './BurgerNav.module.scss';
+import {Link} from "react-scroll";
+
+export const BurgerNav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className={style.burgerNav}>
+            <div className={isOpen ? `${style.burgerNavItems} ${style.show}` : style.burgerNavItems}>
+                <a href="/#">Main</a>
+                <Link to="skills"
+                      activeClass={style.active}
+                      spy={true}
+                      smooth={true}
+                      offset={1}
+                      duration={500}
+                >
+                    Skills
+                </Link>
+                <Link to="projects"
+                      activeClass={style.active}
+                      spy={true}
+                      smooth={true}
+                      offset={1}
+                      duration={500}
+                >
+                    Projects
+                </Link>
+                <Link to="contacts"
+                      activeClass={style.active}
+                      spy={true}
+                      smooth={true}
+                      offset={1}
+                      duration={500}
+                >
+                    Contacts
+                </Link></div>
+            <div className={style.burgerBtn} onClick={() => setIsOpen(!isOpen)}></div>
+        </div>
+    );
+}
