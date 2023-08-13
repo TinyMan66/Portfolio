35e6df081290239b981210524461037fdd0edc59
Diana Lipanova <altere2804@gmail.com>
2023-08-13 21:47:07 +0300

    refactor: skills description

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -14,16 +14,11 @@ export const Skills = () => {
                 <div className={style.skillsContainer}>
                     <Title text={"My Skills"}/>
                     <div className={style.skills}>
-                        <Skill icon={faJs} title={"JavaScript"}
-                               description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}/>
-                        <Skill icon={faCss3Alt} title={"CSS"}
-                               description={"It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}/>
-                        <Skill icon={faReact} title={"React"}
-                               description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}/>
-                        <Skill icon={faHtml5} title={"Html"}
-                               description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}/>
-                        <Skill iconImg={tsIcon} title={"TypeScript"}
-                               description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}/>
+                        <Skill icon={faJs} title={"JavaScript"}/>
+                        <Skill icon={faCss3Alt} title={"CSS"}/>
+                        <Skill icon={faReact} title={"React"}/>
+                        <Skill icon={faHtml5} title={"Html"}/>
+                        <Skill iconImg={tsIcon} title={"TypeScript"}/>
                     </div>
                 </div>
             </Fade>
